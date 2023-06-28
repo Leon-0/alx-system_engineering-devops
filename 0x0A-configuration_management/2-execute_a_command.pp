256fa35ce3ba77bb4ee95037eba007bb5853a47f
Jesuferanmi Omoniyi <lyoncbe@gmail.com>
2023-06-28 11:36:45 +0100

    Executing a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,10 +1,7 @@
 # Define the exec resource to kill the process
 
 exec { 'killmenow':
-  # Specify the command to be executed
   command     => 'pkill killmenow',
-  # Ensure the process is killed only when the manifest is applied
   refreshonly => true,
-  # Only execute the command if the process is running
   onlyif      => 'pgrep killmenow',
 }
