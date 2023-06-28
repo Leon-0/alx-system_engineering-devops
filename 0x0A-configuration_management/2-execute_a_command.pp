4092690f99d8bad0a78942adc40c795d07e8cedd
Jesuferanmi Omoniyi <lyoncbe@gmail.com>
2023-06-28 12:09:06 +0100

    puppet

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,9 @@
 # kill a process named 'killmenow'
 
-exec { 'kill killmenow':
-  command     => 'pkill -f killmenow',
-  path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-  onlyif      => 'pgrep -f killmenow',
+# Define the exec resource to kill the process
+exec { 'killmenow':
+  command     => 'pkill killmenow',
+  refreshonly => true,
+  onlyif      => 'pgrep killmenow',
 }
+
